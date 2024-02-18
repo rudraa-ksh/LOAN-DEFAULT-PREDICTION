ce390c9e47d028a751850b033b46d7bff389c695
rudraa-ksh <rudrak04sh@gmail.com>
2024-02-18 20:00:48 +0530

    Complete: Model Integration

--- main/base/urls.py
+++ main/base/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name= "home" ),
-    path('result', views.predict, name= "predict"),
+    path('', views.predict, name= "predict" ),
 ]
--- main/base/views.py
+++ main/base/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 import pickle
-model=pickle.load(open('./Model/file.pkl','rb'))
+import numpy as np
+import os
+import sys
+
+model=pickle.load(open('Model/file.pkl','rb'))
 # Create your views here.
-def home(request):
-    return render(request,'base/home.html')
 
 def predict(request):
-    age = request.GET['age']
-    RevolvingUtilizationOfUnsecuredLines = request.GET['RevolvingUtilizationOfUnsecuredLines']
-    DebtRatio =request.GET['DebtRatio']
-    MonthlyIncome = request.GET['MonthlyIncome']
-    NumberOfOpenCreditLinesandLoans = request.GET['NumberOfOpenCreditLinesAndLones']
-    NumberofRealEstateLoansOrLines = request.GET['NumberOfRealEstateLoansOrLones']
-    NumberofDependents = request.GET['NumberOfDependents']
-    result = model.predict_proba(['DebtRatio', 'MonthlyIncome', 'RevolvingUtilizationOfUnsecuredLines','age', 'NumberOfOpenCreditLinesAndLoans','NumberRealEstateLoansOrLines', 'NumberOfDependents'])
-    return render(request, 'base/Result.html',{'result':result})
+    if request.method == 'GET':
+        return render(request, 'base/Home.html')
+    elif request.method == 'POST':
+        age = int(request.POST['age'])
+        RevolvingUtilizationOfUnsecuredLines = float(request.POST['RevolvingUtilizationOfUnsecuredLines'])
+        DebtRatio = float(request.POST['DebtRatio'])
+        MonthlyIncome = float(request.POST['MonthlyIncome'])
+        NumberOfOpenCreditLinesAndLoans = int(request.POST['NumberOfOpenCreditLinesAndLoans'])
+        NumberRealEstateLoansOrLines = int(request.POST['NumberRealEstateLoansOrLines'])
+        NumberOfDependents = float(request.POST['NumberOfDependents'])
+        final = np.array([[DebtRatio, MonthlyIncome, RevolvingUtilizationOfUnsecuredLines, age, NumberOfOpenCreditLinesAndLoans, NumberRealEstateLoansOrLines, NumberOfDependents]])
+        result = model.predict(final)
+        if(result[0] == 1):
+            result ="Your Loan will not be approved"
+        else:
+            result = "Your Loan will be approved"
+        return render(request, 'base/Home.html',{'result':result})
